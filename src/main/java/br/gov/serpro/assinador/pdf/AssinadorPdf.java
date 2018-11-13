a174a97ed28f116397c269b0a8f9e00cbd35b1da
Estêvão "Steven" Chaves Monteiro <estevao@gmail.com>
2018-11-13 17:42:28 -0300

    Update AssinadorPdf.java

--- src/main/java/br/gov/serpro/assinador/pdf/AssinadorPdf.java
+++ src/main/java/br/gov/serpro/assinador/pdf/AssinadorPdf.java
@@ -1,3 +1,35 @@
+/*
+ * AssinadorPdf
+ * Copyright (C) 2018 SERPRO
+ * ----------------------------------------------------------------------------
+ * RubricaClient is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License version 3
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License version 3
+ * along with this program; if not,  see <http://www.gnu.org/licenses/>
+ * or write to the Free Software Foundation, Inc., 51 Franklin Street,
+ * Fifth Floor, Boston, MA  02110-1301, USA.
+ * ----------------------------------------------------------------------------
+ * RubricaClient é um software livre; você pode redistribuí-lo e/ou
+ * modificá-lo dentro dos termos da GNU LGPL versão 3 publicada pela Fundação
+ * do Software Livre (FSF).
+ *
+ * Este programa é distribuído com a esperança que possa ser útil, mas SEM GARANTIA
+ * ALGUMA; sem sequer uma garantia implícita de ADEQUAÇÃO a qualquer MERCADO ou
+ * APLICAÇÃO ESPECÍFICA. Veja a Licença Pública Geral GNU/LGPL em português
+ * para mais detalhes.
+ *
+ * Você deve ter recebido uma cópia da GNU LGPL versão 3, sob o título
+ * "LICENCA.txt", junto com esse programa. Caso contrário, acesse 
+ * <http://www.gnu.org/licenses/> ou escreva para a Fundação do Software Livre (FSF) 
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02111-1301, USA.
+ */
 package br.gov.serpro.assinador.pdf;
 
 import java.io.ByteArrayOutputStream;
