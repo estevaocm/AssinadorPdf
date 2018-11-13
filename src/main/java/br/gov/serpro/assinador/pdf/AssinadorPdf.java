6561031d1d3b75bc3a99d2ec04fce1e6a86b8881
Estêvão "Steven" Chaves Monteiro <estevao@gmail.com>
2018-11-13 17:41:31 -0300

    Update AssinadorPdf.java

--- src/main/java/br/gov/serpro/assinador/pdf/AssinadorPdf.java
+++ src/main/java/br/gov/serpro/assinador/pdf/AssinadorPdf.java
@@ -1,35 +1,3 @@
-/*
- * AssinadorPdf
- * Copyright (C) 2018 SERPRO
- * ----------------------------------------------------------------------------
- * RubricaClient is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public License version 3
- * as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License version 3
- * along with this program; if not,  see <http://www.gnu.org/licenses/>
- * or write to the Free Software Foundation, Inc., 51 Franklin Street,
- * Fifth Floor, Boston, MA  02110-1301, USA.
- * ----------------------------------------------------------------------------
- * RubricaClient é um software livre; você pode redistribuí-lo e/ou
- * modificá-lo dentro dos termos da GNU LGPL versão 3 publicada pela Fundação
- * do Software Livre (FSF).
- *
- * Este programa é distribuído com a esperança que possa ser útil, mas SEM GARANTIA
- * ALGUMA; sem sequer uma garantia implícita de ADEQUAÇÃO a qualquer MERCADO ou
- * APLICAÇÃO ESPECÍFICA. Veja a Licença Pública Geral GNU/LGPL em português
- * para mais detalhes.
- *
- * Você deve ter recebido uma cópia da GNU LGPL versão 3, sob o título
- * "LICENCA.txt", junto com esse programa. Caso contrário, acesse 
- * <http://www.gnu.org/licenses/> ou escreva para a Fundação do Software Livre (FSF) 
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02111-1301, USA.
- */
 package br.gov.serpro.assinador.pdf;
 
 import java.io.ByteArrayOutputStream;
@@ -40,7 +8,6 @@ import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.security.GeneralSecurityException;
 import java.security.MessageDigest;
-import java.util.Base64;
 import java.util.Calendar;
 
 import org.apache.commons.io.IOUtils;
@@ -50,6 +17,7 @@ import org.apache.pdfbox.pdmodel.interactive.digitalsignature.PDSignature;
 import org.apache.pdfbox.pdmodel.interactive.digitalsignature.SignatureOptions;
 import org.apache.pdfbox.pdmodel.interactive.digitalsignature.visible.PDVisibleSigProperties;
 import org.apache.pdfbox.pdmodel.interactive.digitalsignature.visible.PDVisibleSignDesigner;
+import org.bouncycastle.util.encoders.Base64;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -202,7 +170,7 @@ public class AssinadorPdf {
 		md.update(content);
 		//salvarTesteTxt(content);
 		byte[] hash = md.digest();
-		L.info(new String(Base64.getEncoder().encode(hash), StandardCharsets.UTF_8));
+		L.info(Base64.toBase64String(hash));
 		return hash;
 	}
 
